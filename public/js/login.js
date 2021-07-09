f61902d422da09cc10fa1ccb129a47c97a253778
nimout mang <nimout.mang@student.passerellesnumeriques.org>
2021-07-09 20:41:48 +0700

    dialog-design

--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,19 @@
-let login = (event) =>{
-    event.preventDefault();
-    let userName = document.querySelector(".input_user").value;
-   console.log(userName)
-//     let URL = rootEndpoinde + "/post";
-//     axios
-//         .get(URL)
-//         .then(res =>{
-//             let users = res.data;
-//             console.log(users)
-//             let islogin = false;
-//             for (let user of users){
-//                 if (user.authorname === userName && !islogin){
-//                     localStorage.setItem("name", user.authorname);
-//                     islogin = true;
-//                 }
-//             }
-//             if(islogin){
-//                 alert("Login success!")
-//                 window.location.href = rootEndpoinde + "/index.html";
-//             }
-//         })
- }
 
+let app = new Vue({
+    el: "#app",
+    data: {
+        newName: ""
+    },
+    methods: {
+        addName () {
+            this.newName;
+            localStorage.setItem("name",this.newName);
+            window.location.href = "/app.html";  
+    },
+    logout(){
+        window.location.href = "/index.html";
+    }
+}
 
-const btnLogin = document.querySelector("#button");
-// const rootEndpoinde = "http://localhost:3000";
-btnLogin.addEventListener("click", login);
+            
+})
